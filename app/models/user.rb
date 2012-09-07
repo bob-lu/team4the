03dba661a9a680a9a0ac92119c212d55074b8b8a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-09-07 14:09:11 +0200

    Added secure password to User model and fixed so password_confirmation and password is accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :nike_id, :nike_password, :password_digest, :username
-
+  has_secure_password
+  validates_presence_of :password, :on => :create
+  attr_accessible :username, :password_confirmation, :password
   has_many :workouts
   has_many :achievement_points
 end
