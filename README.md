37e53ca1120dd326ff557db1abde987a81878f76
Björn Lennartsson <bjorn@isotop.se>
2012-09-07 11:59:20 +0200

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-fit4the
-=======
-
-A web app for Isotop's competition Fit 4 The
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Team4the::Application.config.secret_token = 'e01fe3bfa7171668fda683881785d73e298858e39661a720a4e096efeb32a085c23a842073eba8cf88a68ed647e75a80ed0c15fd3365bc8aacdfe5b73f29fa26'
