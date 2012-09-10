6ce5025d332d64caa8bc6dae5f0935413a4da539
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-09-10 09:52:23 +0200

    Improved user profile. Updated navigation items and fixed so we can update Nike id and so in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
-  attr_accessible :username, :password_confirmation, :password
+  attr_accessible :username, :password_confirmation, :password, :nike_id, :nike_password
   has_many :workouts
   has_many :achievement_points
   belongs_to :team
