59b5a673d11a3818c76661c950b1fe761dff31a7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-09-14 16:03:55 +0200

    Added edit view to workout. Fixed stupid workout routes!

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -19,6 +19,22 @@ class WorkoutController < ApplicationController
 		end
 	end
 	
+	def edit
+		@workout = Workout.find(params[:id])
+	end
+	
+	def update
+		@workout = Workout.find(params[:id])
+		
+		respond_to do |format|
+			if @workout.update_attributes(params[:workout])
+				format.html { redirect_to @workout, notice: 'Workout was successfully updated.' }
+			else
+				format.html { render action: "edit" }
+			end
+		end
+	end
+	
 	def show
 		@workout = Workout.find(params[:id])
 		
