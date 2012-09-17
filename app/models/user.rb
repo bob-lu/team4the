c0d5db63e6cc3cc2c574291a28e374c3b0ab2a5e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2012-09-17 10:24:00 +0200

    Added nike plus gem and method to access it from a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  require 'nike'
   has_secure_password
   validates_presence_of :password, :on => :create
   attr_accessible :username, :password_confirmation, :password, :nike_id, :nike_password
   has_many :workouts
   has_many :achievement_points
   belongs_to :team
+  
+  def nike
+    nike_connection
+    @c
+  end
+  
+  private
+  
+  def nike_connection
+    @c = ::Nike::Client.new(self.nike_id, self.nike_password) if @c.nil?
+  end
 end
